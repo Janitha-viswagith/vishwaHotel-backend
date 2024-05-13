effede4ec4844e2606d333f04f817824d37cfa01
Janitha vishwagith <132135987+Janitha-viswagith@users.noreply.github.com>
2024-05-13 09:24:24 +0530

    room add done

--- vishwaHotel/src/main/java/com/example/vishwahotel/controller/RoomController.java
+++ vishwaHotel/src/main/java/com/example/vishwahotel/controller/RoomController.java
@@ -6,15 +6,13 @@ import com.example.vishwahotel.service.IRoomService;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +22,7 @@ public class RoomController {
 
    private final IRoomService roomService;
 
-   @PostMapping("/add/new-room")
+   @PostMapping("/add/newRoom")
    public ResponseEntity <RoomResponse> addNewRoom(
          @RequestParam("photo") MultipartFile photo ,
          @RequestParam("roomType")  String roomType ,
@@ -39,4 +37,13 @@ public class RoomController {
 
            return ResponseEntity.ok(response);
    }
+
+   @GetMapping("/room/types")
+public List<String> getRoomTypes(){
+       return roomService.getAllRoomTypes();
+}
+
+
+
+
 }
